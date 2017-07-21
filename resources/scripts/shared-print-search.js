32242708524bfc542e09197634c96ee53eda0733
Alister Fenix <a.fenix@icloud.com>
2017-07-20 17:00:41 -0700

    WIP: update searchSolrTitles() to accept data from new CloudSearch DB
    Functions as expected (grabs data, populates html), but will need new parser for new data format

--- resources/scripts/shared-print-search.js
+++ resources/scripts/shared-print-search.js
@@ -43,52 +43,58 @@ function parseTitleResults(data, groupID,parsedData) {
 function searchSolrTitles() {
     var parsedData = {};
     var rawUserInput = document.getElementById("searchInput").value;
-    var cleanUserInput = rawUserInput.split(' ').join(' AND ');
+    var row = "";
     $([185,191,258,300,302,323]).each(function() {
-      var groupID = this.valueOf();
-      $.getJSON("http://prodsolrcloud-1947786843.us-east-1.elb.amazonaws.com:8983/solr/groupProject" + groupID + "TitleHoldings/select?q=retention_allocated:true&fq=in_scope:TRUE&fq=title:(" + cleanUserInput + ")&wt=json&json.wrf=?&indent=true", function(result) {  
-        var finalParsedData = parseTitleResults(result,groupID, parsedData);
-        var row = "";
-        for (var key in finalParsedData) {
-            var groupArray = finalParsedData[key].groups;
-            var groupLength = finalParsedData[key].groups.length;
-            var worldCatUrl = 'http://worldcat.org/oclc/' + key;
+        var groupID = this.valueOf();
+        $.getJSON("http://shared-print.scs.argenticsoftware.com/search/?q=" + rawUserInput + "&return=_all_fields", function(result) {
+        $("#solr_results").html("");
+        for (var holding_index in result.hits.hit) {
+            var holding = result.hits.hit[holding_index].fields;
+            if (!holding) continue;
+            row +="<li class='search__item'><div class='search__item__desc'><strong class='search__author'>"+ holding.full_author + 
+            "</strong>, <em class='search__title'>" + holding.full_title + 
+            "</span><ul class='search__groups'>"; 
 
-            row+="<li class='search__item'><div class='search__item__desc'><strong class='search__author'>"+ finalParsedData[key].author + 
-            "</strong>, <em class='search__title'>" + finalParsedData[key].title + "</em><span class='search__edition'>" + finalParsedData[key].edition + 
-            "</span><span class='search__publisher'>" + finalParsedData[key].publisher + "</span><span class='search__date'>" + 
-            finalParsedData[key].pub_year + "</span><ul class='search__groups'>"; 
-            for (var i = 0; i < finalParsedData[key].groups.length; i++) {
-                if (finalParsedData[key].groups[i] == "EAST") {
-                    var east = "<li class='search__groups__item east'>" + finalParsedData[key].groups[i] + "<span class='numeral'>15yr</span></li>";  
+            // group_ids: 
+            // 1013;"EAST Group - June 2015"
+            // 1014;"MI-SPI - August 2015"
+            // 5761;"COPPUL"
+            // 8627;"ALI - September 2016"
+            // 9003;"SCELC"
+            // 9808;"MMS - April 2017"
+
+            for (var index = 0; index < holding.group_ids.length; index++) {
+                var group_inst_id = holding.group_ids[index];
+                if (group_inst_id == 1013) {
+                    var east = "<li class='search__groups__item east'>" + "EAST" + "<span class='numeral'>15yr</span></li>";  
                     row+= east;
                 }
-                if (finalParsedData[key].groups[i] == "MI-SPI") {
-                    var mispi = "<li class='search__groups__item mi-spi'>" + finalParsedData[key].groups[i] + "<span class='numeral'>15yr</span></li>"; 
+                if (group_inst_id == 1014) {
+                    var mispi = "<li class='search__groups__item mi-spi'>" + "MI-SPI" + "<span class='numeral'>15yr</span></li>"; 
                     row+= mispi;
                 } 
-                if (finalParsedData[key].groups[i] == "COPPUL") {
-                    var coppul = "<li class='search__groups__item coppul'>" + finalParsedData[key].groups[i] + "<span class='numeral'>15yr</span></li>";
+                if (group_inst_id == 5761) {
+                    var coppul = "<li class='search__groups__item coppul'>" + "COPPUL" + "<span class='numeral'>15yr</span></li>";
                     row+= coppul;  
                 }
-                if (finalParsedData[key].groups[i] == "ALI") {
-                    var ali = "<li class='search__groups__item ali'>" + finalParsedData[key].groups[i] + "<span class='numeral'>15yr</span></li>";
+                if (group_inst_id == 8627) {
+                    var ali = "<li class='search__groups__item ali'>" + "ALI" + "<span class='numeral'>15yr</span></li>";
                     row+= ali;
                 }
-                if (finalParsedData[key].groups[i] == "SCELC") {
-                    var scelc = "<li class='search__groups__item scelc'>" + finalParsedData[key].groups[i] + "<span class='numeral'>15yr</span></li>"; 
+                if (group_inst_id == 9003) {
+                    var scelc = "<li class='search__groups__item scelc'>" + "SCELC" + "<span class='numeral'>15yr</span></li>"; 
                     row+= scelc; 
                 } 
-                if (finalParsedData[key].groups[i] == "MMS") {
-                    var mms = "<li class='search__groups__item mms'>" + finalParsedData[key].groups[i] + "<span class='numeral'>15yr</span></li>"; 
+                if (group_inst_id == 9808) {
+                    var mms = "<li class='search__groups__item mms'>" + "MMS" + "<span class='numeral'>15yr</span></li>"; 
                     row+= mms;
                 }
-            } 
-
-            row+= "</ul></div><a href='" + worldCatUrl + 
+            }
+console.log(holding.worldcat_oclc_nbr);
+            row+= "</ul></div><a href='http://worldcat.org/oclc/" + holding.worldcat_oclc_nbr +
             "' class='search__item__link' target='_blank' rel='noopener noreferrer'><img src='resources/images/WorldCat_Logo_V_Color.png' alt='WorldCat Logo' width='48'></a></li>" ;
         }
-        $("#solr_results").html(row);    
+        $("#solr_results").append(row);    
       });
     });
 }
